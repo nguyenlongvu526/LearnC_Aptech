5e87862a01f7617dd12bb8fde544a4ab36bfccaa
nguyễn long vũ <vunlth2208012@fpt.edu.vn>
2022-08-31 08:11:15 +0700

    Create main3.c

---
+++ 29-08/main3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a = 10;
+	float b = 24.67892345;
+	char ch = 'a';
+	printf("Integer data = %d", a);
+	printf("Float Data = %f ,b");
+	printf("Character = %c",ch);
+	printf("This prints the string");
+	printf("%s","This also prints a string");
+	
+	return 0;
+}
